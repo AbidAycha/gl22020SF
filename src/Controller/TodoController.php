912cd1939579f630269e542e04d4b471ffd0c7ca
aymen <aymen.sellaouti@gmail.com>
2020-05-01 13:14:35 +0100

    first commit

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use MongoDB\Driver\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class TodoController
+ * @package App\Controller
+ * @Route("/todo")
+ */
 class TodoController extends AbstractController
 {
     /**
-     * @Route("/todo", name="todo")
+     * @Route("/", name="todo")
      */
     public function index(Request $request)
     {
@@ -34,7 +40,8 @@ class TodoController extends AbstractController
     }
 
     /**
-     * @Route("/todo/add/{name}/{description}", name="todo.add")
+     * @Route("/add/{name}/{description}", name="todo.add"
+     * )
      */
     public function add(SessionInterface $session, $name, $description) {
         /**
@@ -60,6 +67,74 @@ class TodoController extends AbstractController
         } else {
             $this->addFlash('error', "Vous devez initiliser d'abord la session");
         }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/update/{name}/{description}", name="todo.update")
+     */
+    public function update(SessionInterface $session, $name, $description) {
+        /**
+         * vérifier si la session est crée
+         *      Si oui
+         *          si on a deja un todo avec le meme nom
+         *              si oui
+         *                  erreur
+         *              sinon
+         *                  ajout
+         *     Si non
+         *          erreur
+         */
+        If ($session->has('todos')) {
+            $todos = $session->get('todos');
+            if(!isset($todos[$name])) {
+                $this->addFlash('error', "le Todo $name n'existe pas");
+            } else {
+                $todos[$name]=$description;
+                $session->set('todos', $todos);
+                $this->addFlash('success', "le Todo $name mis à jour avec succès");
+            }
+        } else {
+            $this->addFlash('error', "Vous devez initiliser d'abord la session");
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/delete/{name}", name="todo.delete")
+     */
+    public function delete(SessionInterface $session, $name) {
+        /**
+         * vérifier si la session est crée
+         *      Si oui
+         *          si on a deja un todo avec le meme nom
+         *              si oui
+         *                  erreur
+         *              sinon
+         *                  ajout
+         *     Si non
+         *          erreur
+         */
+        If ($session->has('todos')) {
+            $todos = $session->get('todos');
+            if(!isset($todos[$name])) {
+                $this->addFlash('error', "le Todo $name n'existe pas");
+            } else {
+                unset($todos[$name]);
+                $session->set('todos', $todos);
+                $this->addFlash('success', "le Todo $name a été supprimé avec succès");
+            }
+        } else {
+            $this->addFlash('error', "Vous devez initiliser d'abord la session");
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/reset", name="todo.reset")
+     */
+    function resetTodos(SessionInterface $session) {
+        $session->clear();
         return $this->redirectToRoute('todo');
     }
 }
---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('price', [$this, 'formatPrice']),
+            new TwigFilter('randomName', [$this, 'getRandomName'])
+        ];
+    }
+
+    public function formatPrice($number, $decimals = 0, $decPoint = '.', $thousandsSep = ',')
+    {
+        $price = number_format($number, $decimals, $decPoint, $thousandsSep);
+        $price = '$' . $price;
+        return $price;
+    }
+
+    public function getRandomName() {
+        return 'aymen';
+    }
+
+}
